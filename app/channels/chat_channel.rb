0f44546751423373db8dcde61c75df85674de95d
AZIZXlaouiti <azizrocke@gmail.com>
2021-10-31 14:41:27 -0400

    small changes

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,32 +1,30 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from 'chat_channel'
-    
+    stream_from "chat_channel"
+
     @user = User.find_by(username: params[:user])
     @user.appear
-    ActionCable.server.broadcast "chat_channel"  ,  message: 
-    "#{params[:user]} has joined !"
+    ActionCable.server.broadcast "chat_channel", message: "#{params[:user]} has joined !"
   end
 
   def unsubscribed
     puts "unsubscribing now!"
     @user = User.find_by(username: params[:user])
     @user.disappear
-    ActionCable.server.broadcast "chat_channel"  ,  message: 
-    "#{params[:user]} has left !"
+    ActionCable.server.broadcast "chat_channel", message: "#{params[:user]} has left !"
     # Any cleanup needed when channel is unsubscribed
   end
+
   def create(opts)
-    #this will get called first when sending data back to channel 
+    #this will get called first when sending data back to channel
     #opts = {"content"=>"up", "action"=>"create"}
     #params = {"channel"=>"ChatChannel"}
     ChatMessage.create(
-      user_id: 1,
+      user_id:opts["user_id"],
       room_id: 1,
-      content: opts.fetch('content')
+      content: opts.fetch("content"),
     )
-    
   end
 end
 =begin
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,25 +1,24 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :authorized, only: [:login]
-    def login 
-        @user = User.find_by(username: params[:username])
-        if !@user.online
-            if @user&.authenticate(params[:password])
-              @payload = {user_id: @user.id}
-              @token = encode_token(@payload) #payload, app_secret , algo
-              #token =  # jwt string:
-              @user.update(online: true)
-              render json: {
-                  user: UserSerializer.new(@user),
-                  token: @token,
-              }, status: :accepted 
-              else 
-                  render json: { errors: 'Invalid username or password' }, status: :unauthorized  
-          end
-        else  
-            render json: {errors: 'User is connected already'} , status: :unauthorized 
-        end
-        # @user = User.where("username = ?", params[:username])
-        
-     end
-   
+  skip_before_action :authorized, only: [:login]
+
+  def login
+    @user = User.find_by(username: params[:username])
+    if !@user.online
+      if @user&.authenticate(params[:password])
+        @payload = { user_id: @user.id }
+        @token = encode_token(@payload) #payload, app_secret , algo
+        #token =  # jwt string:
+        render json: {
+                 user: UserSerializer.new(@user),
+                 token: @token,
+               }, status: :accepted
+      else
+        render json: { errors: "Invalid username or password" }, status: :unauthorized
+      end
+    else
+      render json: { errors: "User is already connected " }, status: :unauthorized
+    end
+    # @user = User.where("username = ?", params[:username])
+
+  end
 end
