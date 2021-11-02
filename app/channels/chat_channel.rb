b4d41675b84b78c08898725791b160805c7bb179
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-01 22:24:38 -0400

    fixing small bugs

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,5 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     stream_from "chat_channel"
     @user = User.find_by(username: params[:user])
     @user.appear
@@ -14,18 +13,9 @@ class ChatChannel < ApplicationCable::Channel
     @user.disappear
     @room = Room.first.users.select{|e| e.online}.uniq
     ActionCable.server.broadcast "chat_channel", leave: "#{params[:user]} has left !" , room: @room
-    # ChatMessage.create(
-    #   user_id:@user.id,
-    #   room_id: 1,
-    #   content: "#{params[:user]} has left !",
-    # )
-    # Any cleanup needed when channel is unsubscribed
   end
 
   def create(opts)
-    #this will get called first when sending data back to channel
-    #opts = {"content"=>"up", "action"=>"create"}
-    #params = {"channel"=>"ChatChannel"}
     ChatMessage.create(
       user_id:opts["user_id"],
       room_id: 1,
